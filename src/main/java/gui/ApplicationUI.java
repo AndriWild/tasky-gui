537b5db0657c18f1284ef860cd432c7061953251
Andri Wild <andri.wild@students.fhnw.ch>
2021-04-29 10:07:13 +0200

    Delete unused folder

---
+++ src/main/java/gui/ApplicationUI.java
@@ -0,0 +1,8 @@
+package gui;
+
+import javafx.scene.layout.BorderPane;
+
+public class ApplicationUI extends BorderPane {
+
+
+}
---
+++ src/main/java/gui/Starter.java
@@ -0,0 +1,29 @@
+package gui;
+
+/**
+ * Hello world!
+ *
+ */
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent rootPane = new ApplicationUI();
+    Scene scene = new Scene(rootPane);
+
+    primaryStage.setTitle("Tasky");
+    primaryStage.setScene(scene);
+    primaryStage.setHeight(800);
+    primaryStage.setWidth(1000);
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    Application.launch();
+  }
+}
